42b9b48aa30fff30f4d27a8528b9278d0255e662
mirkemme <mirkomontaina@gmail.com>
2023-07-18 17:58:19 +0200

    feat: add todo list

---
+++ src/components/index.jsx
@@ -0,0 +1,3 @@
+import TodoList from "./todoList/todoList";
+
+export default TodoList;
---
+++ src/mock/todos.js
@@ -0,0 +1,26 @@
+export const todos = [
+  {
+    id: 0,
+    todo: "Lorem ipsum dolor sit amet.",
+  },
+  {
+    id: 1,
+    todo: "Nostrum unde qui accusamus harum commodi, dolor facilis!",
+  },
+  {
+    id: 2,
+    todo: "Lorem ipsum dolor sit, amet consectetur adipisicing elit.",
+  },
+  {
+    id: 3,
+    todo: "Vitae alias laboriosam eaque reiciendis sequi nostrum.",
+  },
+  {
+    id: 4,
+    todo: "Iusto dolore unde quis odit eius aliquid!",
+  },
+  {
+    id: 5,
+    todo: "Totam ex temporibus, sapiente eum corrupti provident eaque odit",
+  },
+];
